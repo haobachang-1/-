e8e572a42ec8a0b54a679c656a4af76efc81dc74
haobachang-1 <haobachang@126.com>
2025-12-09 22:04:27 +0800

    Add files via upload

---
+++ 2586671f-98b6-4889-a2e8-49968924412e.php
@@ -0,0 +1,9 @@
+<?php
+$flag_file = '/tmp/flag.txt';
+if (file_exists($flag_file)) {
+    $content = file_get_contents($flag_file);
+    echo htmlspecialchars($content, ENT_QUOTES | ENT_SUBSTITUTE, 'UTF-8');
+} else {
+    echo "flag 文件不存在";
+}
+?>
